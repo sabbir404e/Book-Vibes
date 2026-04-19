f8508161450a25b79ff792364b47dc6b96431a32
MD. Sabbir Hossain <sabbirhossain8721@gmail.com>
2026-04-19 16:35:49 +0600

    39-1 Project Setup with React Router, Tailwind and Daisy UI

---
+++ src/pages/books/Books.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Books = () => {
+    return (
+        <div>
+            <h3>This is Books page</h3>
+        </div>
+    );
+};
+
+export default Books;
---
+++ src/pages/hompage/Homepage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Homepage = () => {
+    return (
+        <div>
+            <h3>This is hompage Hompage</h3>
+        </div>
+    );
+};
+
+export default Homepage;
